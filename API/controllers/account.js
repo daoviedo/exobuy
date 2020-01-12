d647adcbeec4c2e74cb91d4a22911de17857dce0
daoviedo <44733952+daoviedo@users.noreply.github.com>
2020-01-12 12:45:15 -0600

    bcrypt/jwt

--- API/controllers/account.js
+++ API/controllers/account.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 //route to get all municipalities
 router.post('/login', async (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    console.log(username, password)
 });
 
 module.exports = router;
