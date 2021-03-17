d542d1118ebe3b8481f4a85eab6792c366c50216
Daniel <daniel206@hotmail.com>
2021-03-16 23:41:46 -0500

    synced files

--- API/config/database.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-//Database connection credentials
-module.exports = {
-  hrPool: {
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    connectString: process.env.DB_CONN
-  }
-};
